e7ceb995b0ccb408202860a39f82c13ef456e473
haseebjanhamraz <haseebjanhamraz@gmail.com>
2024-09-15 18:31:20 +0500

    "Updated dependencies and code in app/layout, app/lib/actions, app/ui/dashboard/sidenav, and app/ui/login-form; bumped next-auth version to 5.0.0-beta.20"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
 
 export default function RootLayout({
   children,
@@ -12,3 +14,14 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  keywords: ['e-commerce', 'acme', 'company'],
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
