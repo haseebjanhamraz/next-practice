d72baf908ccf46bab8828472924d1ebfff8d1140
haseebjanhamraz <haseebjanhamraz@gmail.com>
2024-09-18 02:47:03 +0500

    "Refactor dashboard pages and UI components for customers and invoices"

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,29 @@
-export default function Customer() {
-    return <p>Customer Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import CustomersTable from "@/app/ui/customers/table"
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchFilteredCustomers(query);
+
+    return (
+        <div className="w-full">
+            <CustomersTable customers={await fetchFilteredCustomers(query)} />
+        </div>
+    );
 }
